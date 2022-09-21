fa88617d8532643d1d020698415663142c3df6b5
x1aff <x1affunit@outlook.com>
2022-09-21 15:11:42 +0300

    Добавьте файлы проекта.

---
+++ MindBoxLib/SquareMeter.cs
@@ -0,0 +1,19 @@
+﻿namespace MindBoxLib
+{
+    public class SquareMeter
+    {
+        static double FindSquare(params double[] sides)
+        {
+            switch (sides.Length)
+            {
+                case 1:
+                    return Math.PI * sides[0] * sides[0];
+                case 3:
+                    double p = sides.Sum() / 2;
+                    return Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+                default:
+                    throw new ArgumentException("App doesn`t support this figure");
+            }
+        }
+    }
+}
